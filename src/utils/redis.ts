717c3b10a6d7f253f76f07e003cfc4e6833266c7
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-08-06 23:51:26 +0330

    add redis request

---
+++ src/utils/redis.ts
@@ -0,0 +1,28 @@
+import auth from "@/auth";
+
+const upstashRedisRestUrl = process.env.UPSTASH_REDIS_REST_URL;
+const authToken = process.env.UPSTASH_REDIS_REST_TOKEN;
+
+type Commands = "zrange" | "sismember" | "get" | "smembers";
+
+export async function fetchRedis(
+   command: Commands,
+   ...args: (string | number)[]
+) {
+   const commandUrl = `${upstashRedisRestUrl}/${command}/${args.join("/")}`;
+
+   const response = await fetch(commandUrl, {
+      headers: {
+         Authorization: `Bearer ${authToken}`,
+      },
+      cache: "no-store",
+   });
+
+   if (!response.ok) {
+      throw new Error("Error exucuting redis command: " + response.statusText);
+   }
+
+   const data = await response.json();
+
+   return data.result;
+}
