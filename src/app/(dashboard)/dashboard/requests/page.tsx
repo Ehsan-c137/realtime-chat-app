ea413f35edff098a4a4c69d454286e3ebf9fdeb9
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-08-08 15:05:11 +0330

    init: add requests page

---
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -0,0 +1,48 @@
+import { authOptions } from "@/auth";
+import { fetchRedis } from "@/utils/redis";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import FriendRequests from "@/components/FriendRequests";
+
+import { FC } from "react";
+
+interface PageProps {}
+
+const Page = async () => {
+   const session = await getServerSession(authOptions);
+   if (!session) notFound();
+
+   // ids of people who have sent you a friend request
+   const incomingSenderIds = (await fetchRedis(
+      "smembers",
+      `user:${session.user.id}:incoming_friend_requests`
+   )) as string[];
+
+   const incomingFriendRequest = await Promise.all([
+      ...incomingSenderIds.map(async (senderId) => {
+         const sender = (await fetchRedis("get", `user:${senderId}`)) as string;
+         const senderParsed = JSON.parse(sender) as User;
+
+         return {
+            senderId,
+            senderEmail: senderParsed.email,
+         };
+      }),
+   ]);
+
+   console.log(incomingFriendRequest);
+
+   return (
+      <main className="pt-8">
+         <h1 className="font-bold text-5xl mb-8"></h1>
+         <div className="flex flex-col gap-4">
+            <FriendRequests
+               incomingFriendRequest={incomingFriendRequest}
+               sessionId={session.user.id}
+            />
+         </div>
+      </main>
+   );
+};
+
+export default Page;
