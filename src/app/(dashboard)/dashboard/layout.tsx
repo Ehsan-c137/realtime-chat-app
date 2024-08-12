c16924b3b508e07edcbe305c4d7ba3418e4f7e8f
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-08-12 14:14:23 +0330

    add sidebar chat list

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,38 +1,43 @@
+import { Icon, Icons } from "@/components/Icon";
+import SignOutButton from "@/components/SignOutButton";
 import { authOptions } from "@/auth";
 import { getServerSession } from "next-auth";
-import { notFound } from "next/navigation";
-import { ReactNode } from "react";
-import Link from "next/link";
-import { Icons, TIcon } from "@/components/Icon";
 import Image from "next/image";
-import SignOutButton from "@/components/SignOutButton";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import { FC, ReactNode } from "react";
 import FriendRequestSidebarOption from "@/components/FriendRequestSidebarOption";
 import { fetchRedis } from "@/utils/redis";
+import { getFriendsByUserId } from "@/utils/get-friends-by-user-id";
+import SidebarChatList from "@/components/SidebarChatList";
+import { SidebarOption } from "@/types/typing";
+// import MobileChatLayout from "@/components/MobileChatLayout";
 
 interface LayoutProps {
    children: ReactNode;
 }
 
-type SidebarOption = {
-   id: number;
-   name: string;
-   href: string;
-   icon: TIcon;
+// Done after the video and optional: add page metadata
+export const metadata = {
+   title: "FriendZone | Dashboard",
+   description: "Your dashboard",
 };
 
 const sidebarOptions: SidebarOption[] = [
    {
       id: 1,
-      name: "Add a friend",
+      name: "Add friend",
       href: "/dashboard/add",
-      icon: "UserPlus",
+      Icon: "UserPlus",
    },
 ];
 
-export default async function Layout({ children }: { children: ReactNode }) {
+const Layout = async ({ children }: LayoutProps) => {
    const session = await getServerSession(authOptions);
-
    if (!session) notFound();
+
+   const friends = await getFriendsByUserId(session.user.id);
+   console.log("friends", friends);
 
    const unseenRequestCount = (
       (await fetchRedis(
@@ -42,33 +47,54 @@ export default async function Layout({ children }: { children: ReactNode }) {
    ).length;
 
    return (
-      <div className="w-full flex h-screen bg-white">
-         <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 px-6 py-4 bg-white">
+      <div className="w-full flex h-screen">
+         {/* <div className="md:hidden">
+            <MobileChatLayout
+               friends={friends}
+               session={session}
+               sidebarOptions={sidebarOptions}
+               unseenRequestCount={unseenRequestCount}
+            />
+         </div> */}
+
+         <div className="hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
             <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
                <Icons.Logo className="h-8 w-auto text-indigo-600" />
             </Link>
-            <div className="text-xs font-semibold leading-6 text-gray-400">
-               Your chats
-            </div>
+
+            {friends.length > 0 ? (
+               <div className="text-xs font-semibold leading-6 text-gray-400">
+                  Your chats
+               </div>
+            ) : null}
+
             <nav className="flex flex-1 flex-col">
                <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                  <li> chats taht this user has</li>
+                  <li>
+                     <SidebarChatList
+                        sessionId={session.user.id}
+                        friends={friends}
+                     />
+                  </li>
                   <li>
                      <div className="text-xs font-semibold leading-6 text-gray-400">
                         Overview
                      </div>
-                     <ul role="list" className="mt-2 -mx-2 space-y-1">
+
+                     <ul role="list" className="-mx-2 mt-2 space-y-1">
                         {sidebarOptions.map((option) => {
-                           const Icon = Icons[option.icon];
+                           const Icon = Icons[option.Icon];
+
                            return (
                               <li key={option.id}>
                                  <Link
                                     href={option.href}
-                                    className="text-gray-700 transition-all transition-duration-300 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                                    className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                  >
-                                    <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.65rem] font-medium bg-white">
+                                    <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
                                        <Icon className="h-4 w-4" />
                                     </span>
+
                                     <span className="truncate">
                                        {option.name}
                                     </span>
@@ -76,46 +102,51 @@ export default async function Layout({ children }: { children: ReactNode }) {
                               </li>
                            );
                         })}
+
+                        <li>
+                           <FriendRequestSidebarOption
+                              sessionId={session.user.id}
+                              initialUnseenRequestCount={unseenRequestCount}
+                           />
+                        </li>
                      </ul>
                   </li>
-                  <li>
-                     <FriendRequestSidebarOption
-                        sessionId={session.user.id}
-                        initialUnseenRequestCount={unseenRequestCount}
-                     />
-                  </li>
+
                   <li className="-mx-6 mt-auto flex items-center">
                      <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900">
-                        <div className="relative w-8 h-8 bg-gray-50">
+                        <div className="relative h-8 w-8 bg-gray-50">
                            <Image
                               fill
-                              alt="Your profile picture"
-                              src={session?.user?.image || ""}
+                              referrerPolicy="no-referrer"
                               className="rounded-full"
+                              src={session.user.image || ""}
+                              alt="Your profile picture"
                            />
-                           <span className="sr-only">Your profile</span>
-                           <div className="flex flex-col ">
-                              <span
-                                 className="absolute inset-0 rounded-full"
-                                 aria-hidden="true"
-                              >
-                                 {session?.user?.name}
-                              </span>
-                              <span
-                                 className="text-xs text-zinc-400"
-                                 aria-hidden="true"
-                              >
-                                 {session?.user?.email}
-                              </span>
-                           </div>
+                        </div>
+
+                        <span className="sr-only">Your profile</span>
+                        <div className="flex flex-col">
+                           <span aria-hidden="true">{session.user.name}</span>
+                           <span
+                              className="text-xs text-zinc-400"
+                              aria-hidden="true"
+                           >
+                              {session.user.email}
+                           </span>
                         </div>
                      </div>
+
                      <SignOutButton className="h-full aspect-square" />
                   </li>
                </ul>
             </nav>
          </div>
-         {children}
+
+         <aside className="max-h-screen container py-16 md:py-12 w-full">
+            {children}
+         </aside>
       </div>
    );
-}
+};
+
+export default Layout;
