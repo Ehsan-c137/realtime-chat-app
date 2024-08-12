cbaa6a80faf76ade3369a77502ef260ceda16256
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-08-12 14:14:58 +0330

    add pusher

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -4,6 +4,8 @@ import { fetchRedis } from "@/utils/redis";
 import { db } from "@/lib/db";
 import { z } from "zod";
 import { authOptions } from "@/auth";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 
 export async function POST(req: Request) {
    try {
@@ -54,6 +56,15 @@ export async function POST(req: Request) {
       }
 
       // valid request, send friend request
+      pusherServer.trigger(
+         toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
+         "incoming_friend_requests",
+         {
+            senderId: session.user.id,
+            senderEmail: session.user.email,
+         }
+      );
+
       db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
 
       return new Response("OK");
