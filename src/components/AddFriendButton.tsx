bbfd67cdf846df52971414c902113b926414556d
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-08-15 01:57:18 +0330

    add text-black

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -6,6 +6,7 @@ import axios, { AxiosError } from "axios";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@headlessui/react";
 
 type FormData = z.infer<typeof addFriendValidator>;
 
@@ -62,7 +63,7 @@ export default function AddFriendButton() {
                placeholder="you@example.com"
                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
-            <button> Add</button>
+            <Button className="text-black">Add</Button>
          </div>
          <p className="mt-1 text-sm text-green-600">{errors.email?.message}</p>
          {showSucessMessage && (
