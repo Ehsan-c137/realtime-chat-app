ef54c89308671330fcf93e1c225946b0cc199b65
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-08-16 15:34:29 +0330

    change padding

--- src/app/(dashboard)/dashboard/requests/page.tsx
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -31,7 +31,7 @@ const Page = async () => {
    ]);
 
    return (
-      <main className="pt-8">
+      <main className="pl-8 pt-8">
          <h1 className="font-bold text-5xl mb-8"></h1>
          <div className="flex flex-col gap-4">
             <FriendRequests
