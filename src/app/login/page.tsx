0a38f2c380e298dff084992e29682b4894a41ec4
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-08-16 12:29:50 +0330

    change login style

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,8 @@ import { FC, useState } from "react";
 import Button from "@/components/ui/Button";
 import { signIn } from "next-auth/react";
 import toast from "react-hot-toast";
+import { Icons } from "@/components/Icon";
+import style from "./login.module.css";
 
 const Page: FC = () => {
    const [isLaoding, setIsLoading] = useState(false);
@@ -21,10 +23,12 @@ const Page: FC = () => {
 
    return (
       <>
-         <div className="flex min-h-screen items-center justify-center bg-white py-12 px-4 sm:px-6 lg:px-8">
+         <div
+            className={`${style.background} flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8`}
+         >
             <div className="w-full flex flex-col items-center max-w-md space-y-8">
                <div className="flex flex-col items-center gap-8">
-                  <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                  <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-white">
                      Sign in to your account
                   </h2>
                </div>
@@ -35,7 +39,8 @@ const Page: FC = () => {
                   className="max-w-sm mx-auto w-full bg-blue-500"
                   type="button"
                >
-                  Google - logo
+                  <Icons.GoogleIcon />
+                  Sign in with Google
                </Button>
             </div>
          </div>
