41bb378156d2a86cad415909a619d50fb5ed0f08
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-08-23 11:46:18 +0330

    add description to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
    title: "Realtime chat",
-   description: "",
+   description: "Realtime Chat",
+   manifest: "./manifest.json",
 };
 
 export default function RootLayout({
